f9166df85f851013fe18a151f90d1b4789c989b3
aagneye-syam <saagneye2003@gmail.com>
2023-09-28 23:37:38 +0530

    derived some features

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ from sklearn.metrics import mean_absolute_error as mae
 import warnings
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv('StoreDemand.csv')
+df = pd.read_csv('DecathlonItem.csv')
 display(df.head())
 display(df.tail())
+
+df.shape
+
+df.info()
+
+df.describe()
+
+parts = df["date"].str.split("-", n = 3, expand = True)
+df["year"]= parts[0].astype('int')
+df["month"]= parts[1].astype('int')
+df["day"]= parts[2].astype('int')
+df.head()
